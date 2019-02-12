58b4d1446f83a90db9ee08cd7fb38a4b55d31318
munganyendesandrine <mungasandwingsd@gmail.com>
2019-02-12 12:11:24 +0200

    Password Locker IP

---
+++ userAcc.py
@@ -0,0 +1,14 @@
+class UserAccount:
+    """
+    Class that generates new instances of contacts.
+    """
+
+    userAccount_list = [] # Empty contact list
+
+    def __init__(self,userName,Pword):
+
+      # docstring removed for simplicity
+
+        self.userName = userName
+        self.Pword = Pword
+       
