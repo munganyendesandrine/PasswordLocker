13eb301ec1619f7f19bc380ee8e704170e390a6b
munganyendesandrine <mungasandwingsd@gmail.com>
2019-02-12 19:09:39 +0200

    Credentials class arguments added

---
+++ Credentials.py
@@ -0,0 +1,10 @@
+class Credentials:
+    """
+    Class that generates new instances of credentials.
+    """
+
+    def __init__(self,username,password):
+
+        self.username = username
+        self.password = password
+     
